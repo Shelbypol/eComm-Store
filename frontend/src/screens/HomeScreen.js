acaaedd3620f318d8869a2df18fb6448553fa649
Shelby Polasek <shelbypolasek@gmail.com>
2020-10-09 10:49:26 -0500

    screens folder and product name rendering on Home screen

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Row, Col} from 'react-bootstrap'
+import products from "../products";
+
+const HomeScreen = () =>  {
+    return (
+        <>
+            <h1>Lastest Products</h1>
+            <Row>
+                {products.map(product => (
+                    <Col sm={12} md={6} lg={4}>
+                        <h3>{product.name}</h3>
+                    </Col>
+                ))}
+            </Row>
+
+        </>
+    )
+}
+
+export default HomeScreen
