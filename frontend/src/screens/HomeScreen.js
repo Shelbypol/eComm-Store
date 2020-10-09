7903fcd6190d235ba30a710e72eb691832cdbb19
Shelby Polasek <shelbypolasek@gmail.com>
2020-10-09 11:49:44 -0500

    react cards display dummy product information

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -10,7 +10,7 @@ const HomeScreen = () =>  {
             <Row>
                 {products.map(product => (
                     <Col sm={12} md={6} lg={4}>
-                        <h3>{product.name}</h3>
+                        {/*<h3>{product.name}</h3>*/}
                         <Product product={product}/>
                     </Col>
                 ))}
