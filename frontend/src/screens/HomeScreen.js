4a9d244cb187f46e5c9b48a85ce3abd2e0247b59
Shelby Polasek <shelbypolasek@gmail.com>
2020-10-12 16:20:27 -0500

    key property added to each list item

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -9,8 +9,9 @@ const HomeScreen = () =>  {
             <h1>Latest Products</h1>
             <Row>
                 {/* Map is telling it to loop through each product where it is then bringing in the product component to know how to display each one */}
+                {/* When you map out items like this to create a list each element needs to have a key and that needs to be unique */}
                 {products.map(product => (
-                    <Col sm={12} md={6} lg={4}>
+                    <Col key={product._id} sm={12} md={6} lg={4}>
                         <Product product={product}/>
                     </Col>
                 ))}
