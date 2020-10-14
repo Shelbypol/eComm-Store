41b95321cd0268b82b5ceb92ae236a1611a7cd09
Shelby Polasek <shelbypolasek@gmail.com>
2020-10-14 16:56:05 -0500

    initial backend server with GET req for all products and ind rendering on port 5000

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,18 @@ app.get('/', (req,res) => {
     res.send('API is running...')
 } );
 
-// create API route
-// then set up respond with json (could do send but only json is sent to and from, although send would work because it would convert it to json type )
+// GET ALL products
+// create API route then set up respond with json (could do send but only json is sent to and from, although send would work because it would convert it to json type )
 // and call products which is the variable on line 6 requiring the products.js from the data folder (save and check port 5000/api/products and will see all products in array)
 app.get('/api/products', (req,res) => {
     res.json(products)
+} );
+
+// GET SINGLE product
+app.get('/api/products/:id', (req,res) => {
+    // p stands for singular product much like (p : product) in the new array set up and then req.params reads the url and grabs that product that equals p._id
+    const product = products.find(p => p._id === req.params.id);
+    res.json(product)
 } );
 
 // take variable app and listen on port 5000
