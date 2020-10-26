d8eefd57a42c744f28e058e6b0a52286adcca42c
Shelby Polasek <shelbypolasek@gmail.com>
2020-10-26 16:25:57 -0500

    added controller file not communicating to postman

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,8 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
-
+// import asyncHandler from 'express-async-handler'
 const router = express.Router();
-import Product from '../models/productModel.js'
+// import Product from '../models/productModel.js'
+import { getProducts, getProductById } from '../controllers/productControllers.js'
 
 
 // GET ALL products MOVED FROM SERVER
@@ -10,30 +10,7 @@ import Product from '../models/productModel.js'
 // and call products which is the variable on line 6 requiring the products.js from the data folder (save and check port 5000/api/products and will see all products in array)
 // anything that is /api etc wil get pointed to this file so we can just have /
 
-// @desc    Fetch all products
-// @route   GET /api/products
-// @access  Public
-router.get('/', asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-
-    res.json(products)
-}));
-
-// @desc    Fetch single products
-// @route   GET /api/products/:id
-// @access  Public
-router.get('/:id', asyncHandler(async (req, res) => {
-    // p stands for singular product much like (p : product) in the new array set up and then req.params reads the url and grabs that product that equals p._id
-    const product = await Product.findById(req.params.id);
-
-    // setting status is optional if not set it will automatically be a 500 error
-    if (product) {
-        res.json(product)
-    }else{
-        res.status(404);
-        throw new Error('Product not found')
-    }
-
-}));
-
+// router.route('/').get(getProducts);
+router.get('/', getProducts);
+router.route('/:id').get(getProductById);
 export default router
