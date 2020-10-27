437cd087701a59eba36683e4fd6d05ddc1ac5ade
Shelby Polasek <shelbypolasek@gmail.com>
2020-10-27 16:26:16 -0500

    login screen build

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from "react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { login } from '../actions/userAction'
+import FormContainer from "../components/FormContainer";
 
 const LoginScreen = () => {
     const [email, setEmail] = useState('');
@@ -13,9 +14,30 @@ const LoginScreen = () => {
 
 
     return (
-        <div>
+        <FormContainer>
+            <h1>Sign in</h1>
+            <Form onSubmit={submitHandler}>
+                <Form.Group constolId='email'>
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control type='email'
+                      placeholder='Enter email'
+                                  value={email}
+                                  onChange{(e) => setEmail(e.target.value)}>
+                    </Form.Control>
+                </Form.Group>
 
-        </div>
+                <Form.Group constolId='Password'>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type='Password'
+                                  placeholder='Enter Password'
+                                  value={password}
+                                  onChange{(e) => setPassword(e.target.value)}>
+                    </Form.Control>
+                </Form.Group>
+            </Form>
+
+            </Form>
+        </FormContainer>
     )
 };
 
