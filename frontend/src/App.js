2ce7c160c943dfd9c9420b9003c2b9a5518340aa
Shelby Polasek <shelbypolasek@gmail.com>
2020-10-28 11:22:19 -0500

    registration screen working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,8 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegsiterScreen";
+
 
 const App = () => {
     return (
@@ -20,6 +22,7 @@ const App = () => {
                 <Container>
                     {/* declare route, assign path and what component that path goes to and then 'exact' which means the path must match exactly */}
                     <Route path='/login' component={LoginScreen} />
+                    <Route path='/register' component={RegisterScreen} />
                     <Route path='/product/:id' component={ProductScreen}/>
                     <Route path='/cart/:id?' component={CartScreen} />
                     <Route path='/' component={HomeScreen} exact />
