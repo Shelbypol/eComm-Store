70aad24c9afcf90cfc1b4c7cab229e820e3c60bf
Shelby Polasek <shelbypolasek@gmail.com>
2020-11-09 16:15:43 -0600

    order screen ui rendering

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -9,7 +9,7 @@ import Loader from "../components/Loader";
 import {getOrderDetails, payOrder} from "../actions/orderActions";
 import { ORDER_PAY_RESET } from "../constants/orderConstants";
 
-const OrderScreen = ({match, history}) => {
+const OrderScreen = ({match}) => {
     const orderId = match.params.id;
 
     const [sdkReady, setSdkReady] = useState(false);
