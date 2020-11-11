a16818bde56c31adc577fac6b43cab1dd610f259
Shelby Polasek <shelbypolasek@gmail.com>
2020-11-11 17:22:56 -0600

    edit & delete btns

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -43,7 +43,18 @@ const UserListScreen = () => {
                                         : ( <i className='fas fa-times' style={{color: 'red'}} > </i>
                                         )}
                                 </td>
+                                <td>
+                                    <LinkContainer to={`/user/${user._id}/edit`}>
+                                        <Button variant='light' className='btn-sm'>
+                                            <i className='fas fa-edit'> </i>
+                                        </Button>
+                                    </LinkContainer>
 
+                                    <Button variant='danger' className='btn-sm' onClick={()=> deleteHandler(user._id)}>
+                                        <i className='fas fa-trash'> </i>
+                                    </Button>
+
+                                </td>
                             </tr>
                         ))}
                         </tbody>
