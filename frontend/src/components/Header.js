f75add3188a7fc6a5053c4fd84d4a30639d6b500
Shelby Polasek <shelbypolasek@gmail.com>
2020-11-11 18:13:49 -0600

    admin links

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -41,19 +41,20 @@ const Header = () => {
                                         <NavDropdown.Item>Profile</NavDropdown.Item>
                                     </LinkContainer>
 
-                                    {/*{userInfo && userInfo.isAdmin && (*/}
-                                    {/*    <NavDropdown title='Admin' id='adminmenu'>*/}
-                                    {/*        <LinkContainer to='/admin/userlist'>*/}
-                                    {/*            <NavDropdown.Item>Users</NavDropdown.Item>*/}
-                                    {/*        </LinkContainer>*/}
-                                    {/*        <LinkContainer to='/admin/productlist'>*/}
-                                    {/*            <NavDropdown.Item>Products</NavDropdown.Item>*/}
-                                    {/*        </LinkContainer>*/}
-                                    {/*        <LinkContainer to='/admin/orderlist'>*/}
-                                    {/*            <NavDropdown.Item>Orders</NavDropdown.Item>*/}
-                                    {/*        </LinkContainer>*/}
-                                    {/*  </NavDropdown>*/}
-                                    {/*)}*/}
+                                    {/* want this in the drop down */}
+                                    {userInfo && userInfo.isAdmin && (
+                                        <NavDropdown title='Admin' id='adminmenu'>
+                                            <LinkContainer to='/admin/userlist'>
+                                                <NavDropdown.Item>Users</NavDropdown.Item>
+                                            </LinkContainer>
+                                            <LinkContainer to='/admin/productlist'>
+                                                <NavDropdown.Item>Products</NavDropdown.Item>
+                                            </LinkContainer>
+                                            <LinkContainer to='/admin/orderlist'>
+                                                <NavDropdown.Item>Orders</NavDropdown.Item>
+                                            </LinkContainer>
+                                      </NavDropdown>
+                                    )}
 
                                     <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
                                 </NavDropdown>
