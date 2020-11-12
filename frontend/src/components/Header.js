0efd9e2313675030eccc05f9a020fbf4f124436b
Shelby Polasek <shelbypolasek@gmail.com>
2020-11-11 18:12:20 -0600

    styling header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -40,6 +40,21 @@ const Header = () => {
                                     <LinkContainer to='/profile'>
                                         <NavDropdown.Item>Profile</NavDropdown.Item>
                                     </LinkContainer>
+
+                                    {/*{userInfo && userInfo.isAdmin && (*/}
+                                    {/*    <NavDropdown title='Admin' id='adminmenu'>*/}
+                                    {/*        <LinkContainer to='/admin/userlist'>*/}
+                                    {/*            <NavDropdown.Item>Users</NavDropdown.Item>*/}
+                                    {/*        </LinkContainer>*/}
+                                    {/*        <LinkContainer to='/admin/productlist'>*/}
+                                    {/*            <NavDropdown.Item>Products</NavDropdown.Item>*/}
+                                    {/*        </LinkContainer>*/}
+                                    {/*        <LinkContainer to='/admin/orderlist'>*/}
+                                    {/*            <NavDropdown.Item>Orders</NavDropdown.Item>*/}
+                                    {/*        </LinkContainer>*/}
+                                    {/*  </NavDropdown>*/}
+                                    {/*)}*/}
+
                                     <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
                                 </NavDropdown>
                             ) : <LinkContainer to='/login'>
