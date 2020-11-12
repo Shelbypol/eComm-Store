d661d2afe6482c0aa103d1958914029855f3df56
Shelby Polasek <shelbypolasek@gmail.com>
2020-11-11 18:17:18 -0600

    admin dropdown

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -41,21 +41,6 @@ const Header = () => {
                                         <NavDropdown.Item>Profile</NavDropdown.Item>
                                     </LinkContainer>
 
-                                    {/* want this in the drop down */}
-                                    {userInfo && userInfo.isAdmin && (
-                                        <NavDropdown title='Admin' id='adminmenu'>
-                                            <LinkContainer to='/admin/userlist'>
-                                                <NavDropdown.Item>Users</NavDropdown.Item>
-                                            </LinkContainer>
-                                            <LinkContainer to='/admin/productlist'>
-                                                <NavDropdown.Item>Products</NavDropdown.Item>
-                                            </LinkContainer>
-                                            <LinkContainer to='/admin/orderlist'>
-                                                <NavDropdown.Item>Orders</NavDropdown.Item>
-                                            </LinkContainer>
-                                      </NavDropdown>
-                                    )}
-
                                     <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
                                 </NavDropdown>
                             ) : <LinkContainer to='/login'>
