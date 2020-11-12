98e293e9161669d6e56860ddb11db0ef6b2817c0
Shelby Polasek <shelbypolasek@gmail.com>
2020-11-12 09:10:16 -0600

    confirm delete

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -29,7 +29,9 @@ const UserListScreen = ({ history }) => {
     }, [dispatch, history, successDelete, userInfo]);
 
     const deleteHandler = (id) => {
-        dispatch(deleteUser(id))
+        if(window.confirm('Are you sure you want to delete this user?')){
+            dispatch(deleteUser(id))
+        }
     };
 
 
