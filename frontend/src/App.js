5f97b7328c7ab3e931d27b26c2ac03b0fad0cf04
Shelby Polasek <shelbypolasek@gmail.com>
2020-11-16 14:10:45 -0600

    refactor page num controller/ updated page num action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -44,6 +44,8 @@ const App = () => {
                     <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
                     <Route path='/admin/orderlist' component={OrderListScreen} />
                     <Route path='/search/:keyword' component={HomeScreen} />
+                    <Route path='/page/:pageNumber' component={HomeScreen} exact />
+                    <Route path='/search/:keyword/page/:pageNumber' component={HomeScreen} />
                     <Route path='/' component={HomeScreen} exact />
                 </Container>
             </main>
