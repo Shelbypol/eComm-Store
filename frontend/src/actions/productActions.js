ed5cb9f8bea89d8c6833b30dc00735f352751c0e
Shelby Polasek <shelbypolasek@gmail.com>
2020-11-16 16:14:25 -0600

    carousel component created

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -18,7 +18,10 @@ import axios from 'axios'
      PRODUCT_UPDATE_FAIL,
      PRODUCT_CREATE_REVIEW_SUCCESS,
      PRODUCT_CREATE_REVIEW_REQUEST,
-     PRODUCT_CREATE_REVIEW_FAIL
+     PRODUCT_CREATE_REVIEW_FAIL,
+     PRODUCT_TOP_SUCCESS,
+     PRODUCT_TOP_REQUEST,
+     PRODUCT_TOP_FAIL
  } from '../constants/productConstants'
  // import {ORDER_LIST_MY_FAIL, ORDER_LIST_MY_REQUEST, ORDER_LIST_MY_SUCCESS} from "../constants/orderConstants";
 
@@ -195,6 +198,27 @@ import axios from 'axios'
          dispatch({
              type: PRODUCT_CREATE_REVIEW_FAIL,
              payload: message,
+         })
+     }
+ };
+
+ // /ACTION FOR LIST TOP PRODUCTS
+ export const listTopProducts = () => async (dispatch) => {
+     try {
+         dispatch({ type: PRODUCT_TOP_REQUEST });
+
+         const { data } = await axios.get(`/api/products/top`);
+
+         dispatch ({
+             type: PRODUCT_TOP_SUCCESS,
+             payload: data
+         })
+     } catch(error){
+         dispatch({
+             type: PRODUCT_TOP_FAIL,
+             payload: error.response && error.response.data.message
+                 ? error.response.data.message
+                 : error.message,
          })
      }
  };
