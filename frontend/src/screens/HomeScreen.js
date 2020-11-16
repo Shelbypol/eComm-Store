1c3afed3d0cdd1ae686bddf7563165338da87d4d
Shelby Polasek <shelbypolasek@gmail.com>
2020-11-16 17:04:32 -0600

    meta component title, keyword, descrip props

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
-
 import React, { useEffect } from 'react'
+import { Helmet } from "react-helmet";
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col} from 'react-bootstrap'
 import Product from '../components/Product'
@@ -7,32 +7,9 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { listProducts } from '../actions/productActions'
 import Paginate from '../components/Paginate'
+import Meta from "../components/Meta";
 import ProductCarousel from "../components/ProductCarousel";
-// import axios from 'axios'
-// =========== means starting code before redux
 
-// ===============const HomeScreen = () =>  {
-    // =========== const [products, setProducts] = useState([]);
-    // Use effect hook (make sure to import it)
-    // in hook is where we make axios request (axios was installed in the front end with npm i axios
-    // axios is same thing as fetch api but cleaner http req to our backend to get product info
-    // ===========  useEffect(() => {
-        // returns a promise so could add .then(res but he uses asyn await which requires separate function rather than the syntax on line 14
-        // axios.get('/api/products').then(res...
-        // * ================ const fetchProducts = async () => {
-        // res has a data object attached to it that we have access to it so we can destructure const res to const {data} to use data directly
-        //    need to add a proxy in the front end package.json to tell it to look at port 5000 not 3000 (line 3 in package.json adds a loop back address pointing to 5000)
-        //
-        //     * ============== const { data } = await axios.get('/api/products');
-
-            //change state from empty array to hold data
-            // * ============== setProducts(data)
-        // };
-        // =============fetchProducts()
-    //    add dependencies in empty array to fire off useEffect with additional
-    // }, []);
-
-    // ============================= redux implementation ==================
 const HomeScreen = ({ match }) => {
 
     const keyword = match.params.keyword;
@@ -52,7 +29,7 @@ const HomeScreen = ({ match }) => {
 
     return (
         <>
-
+        <Meta />
             {!keyword && <ProductCarousel /> }
 
             <h1>Latest Products</h1>
