9529bdbd0ec4dcac411546a3ffec2059ddcc12cd
Shelby Polasek <shelbypolasek@gmail.com>
2020-11-16 15:55:21 -0600

    refactored top carousel reducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -124,11 +124,11 @@ export const productReviewCreateReducer = (state = { }, action) => {
 // PRODUCT TOP RATED CAROUSEL REDUCER
 export const productTopRatedReducer = (state = {products: []}, action) => {
     switch(action.type){
-        case PRODUCT_CREATE_TOP_REQUEST:
+        case PRODUCT_TOP_REQUEST:
             return { loading: true, products: [] };
-        case PRODUCT_CREATE_TOP_SUCCESS:
+        case PRODUCT_TOP_SUCCESS:
             return { loading: false, products: action.payload };
-        case PRODUCT_CREATE_TOP_FAIL:
+        case PRODUCT_TOP_FAIL:
             return { loading: false, error: action.payload };
         default:
             return state
