21449099ac00ed9637b353f524c22b48db3e5ef0
Shelby Polasek <shelbypolasek@gmail.com>
2020-11-16 13:51:30 -0600

    product pagination controller set up

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -6,6 +6,10 @@ import Product from '../models/productModel.js'
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
+
+    const pageSize = 2;
+    const page = Number(req.query.pageNumber) || 1;
+
     const keyword = req.query.keyword ? {
         name: {
             $regex: req.query.keyword,
@@ -14,8 +18,9 @@ const getProducts = asyncHandler(async (req, res) => {
     } : {};
 
 
-    const products = await Product.find({ ...keyword});
-    res.json(products)
+    const count = await Product .count({ ...keyword });
+    const products = await Product.find({ ...keyword }).limit(pageSize).skip(pageSize * (page - 1));
+    res.json({ products, page, pages: Math.ceil(count / pageSize)})
 });
 
 
