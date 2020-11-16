e89695eaa65c4447369d800f1b256f0fe1afe81e
Shelby Polasek <shelbypolasek@gmail.com>
2020-11-16 13:53:26 -0600

    product pagination action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -27,11 +27,11 @@ import axios from 'axios'
  // fire this action off in the component
 
  // /ACTION FOR PRODUCT LIST DETAILS
- export const listProducts = (keyword = '') => async (dispatch) => {
+ export const listProducts = (keyword = '', pageNumber = '') => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_LIST_REQUEST });
 
-        const { data } = await axios.get(`/api/products?keyword=${keyword}`);
+        const { data } = await axios.get(`/api/products?keyword=${keyword}&pageNumber=${pageNumber}`);
 
         dispatch ({
             type: PRODUCT_LIST_SUCCESS,
