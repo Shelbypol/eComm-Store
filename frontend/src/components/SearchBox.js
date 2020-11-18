5bac0637808166ad4e5d0776f3ee680e47560e31
Shelby Polasek <shelbypolasek@gmail.com>
2020-11-18 14:43:21 -0600

    refactored reset search bar

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -7,7 +7,7 @@ const SearchBox = ({ history }) => {
         e.preventDefault();
         if(keyword.trim()){
             history.push(`/search/${keyword}`);
-            setKeyword('')
+            // setKeyword('')
         } else {
             history.push('/')
         }
